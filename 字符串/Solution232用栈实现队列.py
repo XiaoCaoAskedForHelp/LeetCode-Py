a6252a238d8f075ef2935fdb52dd001e19db43b3
amos.cao <amos.cao@outlook.com>
2023-07-14 17:57:15 +0800

    Solution232用栈实现队列

---
+++ 字符串/Solution232用栈实现队列.py
@@ -0,0 +1,33 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stackin = []
+        self.stackout = []
+
+    def push(self, x: int) -> None:
+        self.stackin.append(x)
+
+    def pop(self) -> int:
+        if self.empty():
+            return None
+        if self.stackout:
+            return self.stackout.pop()
+        else:
+            for i in range(len(self.stackin)):
+                self.stackout.append(self.stackin.pop())
+            return self.stackout.pop()
+
+    def peek(self) -> int:
+        ans = self.pop()
+        self.stackout.append(ans)
+        return ans
+
+    def empty(self) -> bool:
+        return not (self.stackin or self.stackout)
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
