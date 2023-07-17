5078e65077a230d37abd30590b9f6b2fbeef7e01
amos.cao <amos.cao@outlook.com>
2023-07-17 17:49:27 +0800

    20有效的括号

---
+++ 栈和队列/Solution20有效的括号.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for item in s:
+            if item == '(':
+                stack.append(')')
+            elif item == '{':
+                stack.append('}')
+            elif item == '[':
+                stack.append(']')
+            elif stack and stack[-1] == item:
+                stack.pop()
+            else:
+                return False
+        return not stack
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+        for item in s:
+            if item in mapping.keys():
+                stack.append(mapping[item])
+            elif not stack or stack[-1] != item:
+                return False
+            else:
+                stack.pop()
+        return not stack
