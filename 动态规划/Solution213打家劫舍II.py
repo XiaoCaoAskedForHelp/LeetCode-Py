ad775592814bd7a8872bc7cef1d82582a345c731
amos.cao <amos.cao@outlook.com>
2023-12-29 13:45:59 +0800

    Solution213打家劫舍II

---
+++ 动态规划/Solution213打家劫舍II.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        result1 = self.robRange(nums, 0, len(nums) - 2)  # 情况二
+        result2 = self.robRange(nums, 1, len(nums) - 1)  # 情况三
+        return max(result1, result2)
+
+    def robRange(self, nums: List[int], start: int, end: int):
+        if end == start:
+            return nums[start]
+        prev_max = nums[start]
+        curr_max = max(nums[start], nums[start + 1])
+        for i in range(start + 2, end + 1):
+            temp = curr_max
+            curr_max = max(prev_max + nums[i], curr_max)
+            prev_max = temp
+        return curr_max
