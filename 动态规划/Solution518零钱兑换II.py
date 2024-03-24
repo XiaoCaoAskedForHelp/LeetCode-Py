3c634c2aadb3f34061875159f7554b1cc170485d
amos.cao <amos.cao@outlook.com>
2024-03-24 22:23:40 +0800

    Solution518零钱兑换II.py

--- 动态规划/Solution518零钱兑换II.py
+++ 动态规划/Solution518零钱兑换II.py
@@ -1,9 +1,11 @@
+from cmath import inf
+from functools import cache
 from typing import List
 
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount +1)
+        dp = [0] * (amount + 1)
         dp[0] = 1
         # 遍历物品
         for i in range(len(coins)):
@@ -11,3 +13,32 @@ class Solution:
             for j in range(coins[i], amount + 1):
                 dp[j] += dp[j - coins[i]]
         return dp[amount]
+
+    def coinChange1(self, coins: List[int], amount: int) -> int:
+        dp = [inf] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for coin in coins:
+                # if i - coin >= 0 and (i - coin == 0 or dp[i - coin] != 0):
+                if i - coin >= 0:
+                    dp[i] = min(dp[i - coin] + 1, dp[i])
+        return -1 if dp[-1] == inf else dp[-1]
+
+    def coinChange2(self, coins: List[int], amount: int) -> int:
+        @cache
+        def dfs(amo: int):
+            if amo < 0:
+                return -1
+            if amo == 0:
+                return 0
+            mini = inf
+            for coin in coins:
+                res = dfs(amo - coin)
+                if res >= 0 and res < mini:
+                    mini = res + 1
+            return mini if mini < inf else -1
+
+        return dfs(amount)
+
+
+Solution().coinChange1(coins=[1, 2, 5], amount=11)
---
+++ 周赛390/Solution100245每个字符最多出现两次的最长子字符串.py
@@ -0,0 +1,38 @@
+import collections
+from collections import defaultdict
+
+
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        # 双指针滑动窗口
+        res = 0
+        left = right = 0
+        dic = defaultdict(int)
+        while right < len(s):
+            while right < len(s) and dic[s[right]] <= 1:
+                dic[s[right]] += 1
+                right += 1
+            res = max(res, right - left)
+            if right < len(s):
+                while s[left] != s[right]:
+                    dic[s[left]] -= 1
+                    left += 1
+            left += 1
+            right += 1
+        return res
+
+    # 从每一个位置开始枚举，只要不超过2就可以继续向后
+    def maximumLengthSubstring1(self, s: str) -> int:
+        ans = 0
+        n = len(s)
+        for i in range(n):
+            cnt = collections.Counter()
+            for j in range(i, n):
+                cnt[s[j]] += 1
+                if cnt[s[j]] > 2:
+                    break
+                ans = max(ans, j - i + 1)
+        return ans
+
+
+Solution().maximumLengthSubstring(s="bcbbbcba")
